93752ca7e075e9887d232c2cb719411c817c4dd0
Andreas Roset <andreasroset@gmail.com>
2024-03-14 09:06:40 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,40 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-// Include the required Wire library for I2C<br>#include <Wire.h>
+// Lager variabler for x og y
 int x = 0;
+int y = 0;
 
+// Klassifiserer en funksjon for meldings event
 void receiveEvent(int bytes);
+void requestEvent();
 
 void setup() {
-  // Start the I2C Bus as Slave on address 9
+  // Starter I2C Bus som Slave på address 9
   Wire.begin(9); 
+
+  // Starter Seriel med pc
   Serial.begin(9600);
-  // Attach a function to trigger when something is received.
+
+  // Kopler meldings event ilag med wire.onreceive
   Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
 }
 
 void receiveEvent(int bytes) {
-  x = Wire.read();    // read one character from the I2C
+  x = Wire.read();    // Setter x verdi til verdi fra master melding
+}
+
+void requestEvent() {
+  y = 2 * x;
+  Wire.write(y);
 }
 
 void loop() {
+  Serial.print("Fekk X verdi fra uno: ");
   Serial.println(x);
+
+  Serial.print("Sender y verdi til uno: ");
+  Serial.println(y);
   delay(500);
 }
